1232f7145ff334c051c44c1e3ebb008430038bf2
Kevin Zhao <kevinz8509@gmail.com>
2021-08-18 08:31:24 -0400

    Update discover_plaintext.sh

--- test cases/discover_plaintext.sh
+++ test cases/discover_plaintext.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo tcpdump -i lo -nnX dst port '(23456 or 23457)' | awk '{ if (/!Q#E%T&U8i6y4r2/ || /AUTH/ || /.*0x0030:.*/) { print > "discovered.txt" } else { print > "not-found.txt" } }' &
 sleep 30
 
@@ -6,4 +8,3 @@ if grep -q "!Q#E%T&U8i6y4r2" discovered.txt; then
 else
     echo plaintext password not found
 fi
-
