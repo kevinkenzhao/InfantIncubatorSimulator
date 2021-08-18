5c47921f13a9ab7a9591a8d6f508aad93a343233
Kevin Zhao <kevinz8509@gmail.com>
2021-08-18 09:24:53 -0400

    Update discover_plaintext.sh

--- test cases/discover_plaintext.sh
+++ test cases/discover_plaintext.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#listens for outbound traffic on ports 23456 and 23457, filters for traffic which contains the plaintext password string, redirects affirmative results into discovered.txt, and returns "plaintext password found" if the query for password string is successful.
+
 sudo tcpdump -i lo -nnX dst port '(23456 or 23457)' | awk '{ if (/!Q#E%T&U8i6y4r2/ || /AUTH/ || /.*0x0030:.*/) { print > "discovered.txt" } else { print > "not-found.txt" } }' &
 sleep 30
 
