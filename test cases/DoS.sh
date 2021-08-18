c3704508586dc3555f330e3e4fa196b1f4f7cd8e
Kevin Zhao <kevinz8509@gmail.com>
2021-08-18 08:30:58 -0400

    Update DoS.sh

--- test cases/DoS.sh
+++ test cases/DoS.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-
+# repeatedly submits an authentication request to server and then terminates the spawn netcan session;
 while :
 do
 	echo "AUTH !Q#E%T&U8i6y4r2w" | nc -u 127.0.0.1 23456 &
